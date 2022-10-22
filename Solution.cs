5d3ae7a21176fd9d3b2d1cd897b773e184e6da1e
Dirga Putra <dirga89@gmail.com>
2022-10-22 23:04:46 +0700

    @dirga89 v02 - passed

--- Solution.cs
+++ Solution.cs
@@ -23,7 +23,60 @@ class Result
 
     public static void miniMaxSum(List<int> arr)
     {
-        
+        Int64 resultMax, resultMin;
+        Int64 countMax = 0, countMin = 0;
+        int checkMax = 0, checkMin = 0;
+
+        for(int i=1; i<5; i++)
+        {
+            countMax += arr[i];
+            countMin += arr[i];
+        }
+
+        resultMax = countMax;
+        resultMin = countMin;
+
+        for(int i=0; i<5; i++)
+        {
+            countMax = 0;
+            countMin = 0;
+            for(int j=0; j<5; j++)
+            {
+                if(j == i)
+                {
+
+                }
+                else
+                {
+                    countMax += arr[j];
+                    countMin += arr[j];
+                }
+            }
+            
+            
+            if(countMax > resultMax)
+            {
+                resultMax = countMax;
+                checkMax = i;
+            }
+            else
+            {
+
+            }
+
+            if(countMin < resultMin)
+            {
+                resultMin = countMin;
+                checkMin = i;
+            }
+            else
+            {
+
+            }
+                
+        }
+
+        Console.WriteLine(resultMin.ToString() + " " + resultMax.ToString());
     }
 
 }
@@ -32,8 +85,17 @@ class Solution
 {
     public static void Main(string[] args)
     {
+        List<int> arr = new List<int>();
 
-        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        Console.WriteLine("Masukkan 5 angka:\n");
+
+        for(int i=0; i<5; i++)
+        {
+            Console.WriteLine("Angka Ke-"+ (i+1).ToString()+":");
+            arr.Add(int.Parse(Console.ReadLine()));
+        }
+
+        //List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
 
         Result.miniMaxSum(arr);
     }
